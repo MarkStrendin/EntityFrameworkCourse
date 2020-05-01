9b42a133883280dd0757193ed152527248ff4e0e
Mark Strendin <mark.strendin@gmail.com>
2020-05-01 14:42:27 -0600

    Initial Commit

---
+++ Test.Domain/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Domain
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
