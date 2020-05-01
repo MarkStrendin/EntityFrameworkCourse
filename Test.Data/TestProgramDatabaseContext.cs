95cdbb8e37a3e80b549addbe1345daaf5e592845
Mark Strendin <mark.strendin@gmail.com>
2020-05-01 15:03:49 -0600

    Add a connection string hardcoded like in the course This seems like it's teaching bad practice.

--- Test.Data/TestProgramDatabaseContext.cs
+++ Test.Data/TestProgramDatabaseContext.cs
@@ -10,5 +10,14 @@ namespace Test.Data
     {
         public DbSet<Student> Students { get; set; }
         public DbSet<Contact> Contacts { get; set; }
+
+        // I really don't like how this course has you do this, because it's obviously a bad idea
+        // to hard code the connection string _here_ of all places.
+        // TODO: How would we normally handle setting up the database context?
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Derp.sdf");
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
