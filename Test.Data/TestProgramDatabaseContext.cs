a046a1e72f304a1b73a8ee95cd0d3e20a563dd83
Mark Strendin <mark.strendin@gmail.com>
2020-05-01 15:43:39 -0600

    This seems like a more sane way of handling the database connection string

--- Test.Data/TestProgramDatabaseContext.cs
+++ Test.Data/TestProgramDatabaseContext.cs
@@ -8,16 +8,12 @@ namespace Test.Data
 {
     class TestProgramDatabaseContext : DbContext
     {
+        public TestProgramDatabaseContext(DbContextOptions<TestProgramDatabaseContext> options) 
+            : base(options)
+        { }
+
         public DbSet<Student> Students { get; set; }
         public DbSet<Contact> Contacts { get; set; }
 
-        // I really don't like how this course has you do this, because it's obviously a bad idea
-        // to hard code the connection string _here_ of all places.
-        // TODO: How would we normally handle setting up the database context?
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source=Derp.sdf");
-            base.OnConfiguring(optionsBuilder);
-        }
     }
 }
