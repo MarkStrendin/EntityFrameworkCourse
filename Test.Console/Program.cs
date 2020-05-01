dd350574b4592793a5359343fb6592a441a233c5
Mark Strendin <mark.strendin@gmail.com>
2020-05-01 16:48:08 -0600

    Add some basic code to add stuff to the database

--- Test.Console/Program.cs
+++ Test.Console/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using Test.Data;
+using Test.Domain;
 
 namespace Test.Console
 {
@@ -7,7 +9,33 @@ namespace Test.Console
     {
         static void Main(string[] args)
         {
+            InsertStudent();
+            System.Console.WriteLine("Done!");
+        }
 
+        private static void InsertStudent()
+        {
+            Contact con = new Contact
+            {
+                FirstName = "Harvey",
+                LastName = "Birdman"
+            };
+
+            Student stu = new Student
+            {
+                FirstName = "John",
+                LastName = "Smith",                
+            };
+
+            var optionsBuilder = new DbContextOptionsBuilder<TestProgramDatabaseContext>();
+            optionsBuilder.UseSqlite("Data Source=derp.sdf");
+
+            using (var context = new TestProgramDatabaseContext(optionsBuilder.Options))
+            {
+                context.Add(stu);
+                context.Add(con);
+                context.SaveChanges();
+            }
 
         }
     }
