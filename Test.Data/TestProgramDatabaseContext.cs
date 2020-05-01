a7ff305498b31e39ce1abf5b9b282a1cf89743ea
Mark Strendin <mark.strendin@gmail.com>
2020-05-01 14:53:59 -0600

    Add a contact class, add the start of a dbset

---
+++ Test.Data/TestProgramDatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Test.Domain;
+
+namespace Test.Data
+{
+    class TestProgramDatabaseContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
--- Test.Domain/Contact.cs
+++ Test.Domain/Contact.cs
@@ -4,10 +4,13 @@ using System.Text;
 
 namespace Test.Domain
 {
-    public class Student
+    public class Contact
     {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateTime Birthday { get; set; }
+
+
     }
 }
