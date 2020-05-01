c49d97bc77b1ca7d00ba7eb43f0edfb0b1a082e5
Mark Strendin <mark.strendin@gmail.com>
2020-05-01 15:20:49 -0600

    Add an Id field to student so EF has something to use as a primary key

--- Test.Domain/Student.cs
+++ Test.Domain/Student.cs
@@ -6,6 +6,7 @@ namespace Test.Domain
 {
     public class Student
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime Birthday { get; set; }
